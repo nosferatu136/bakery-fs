3ce89e15b5d2858edd53556b830763a5b42f0c62
Carlos Pardo <nosferatu136@hotmail.com>
2020-07-07 14:20:40 -0500

    Cookies: Sanitize fillings when empty.

--- spec/models/cookie_spec.rb
+++ spec/models/cookie_spec.rb
@@ -10,4 +10,12 @@ describe Cookie do
   describe "validations" do
     it { is_expected.to validate_presence_of(:storage) }
   end
+
+  context 'when cookie has no fillings' do
+    let(:new_cookie) { FactoryGirl.create(:cookie, fillings: '') }
+
+    it 'sanitizes fillings' do
+      expect(new_cookie.fillings).to be_nil
+    end
+  end
 end
