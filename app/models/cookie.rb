36cfe41182a695e31c03733a01501bde39a98549
Carlos Pardo <nosferatu136@hotmail.com>
2020-07-07 13:00:03 -0500

    Cookies: Add ready column and remove ready? method.

--- app/models/cookie.rb
+++ app/models/cookie.rb
@@ -1,9 +1,5 @@
 class Cookie < ActiveRecord::Base
   belongs_to :storage, polymorphic: :true
-  
-  validates :storage, presence: true
 
-  def ready?
-    true
-  end
+  validates :storage, presence: true
 end
